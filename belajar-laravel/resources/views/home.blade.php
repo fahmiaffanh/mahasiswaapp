d71f0d28fad98d9f44a5afa7b2906637ce95ef4c
unknown <fahmiaffanh>
2019-10-01 21:20:45 +0700

    Tambah laravel

---
+++ belajar-laravel/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('main')
+
+@section('title','Home')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>To Do List App</h1>
+        <p>Aplikasi Mencatat Pekerjaan yang akan dikerjakan</p>
+    </div>
+@endsection
---
+++ belajar-laravel/resources/views/list.blade.php
@@ -0,0 +1,7 @@
+@extends('main')
+
+@section('title','List')
+
+@section('content')
+    <h1>List Todo</h1>
+@endsection
